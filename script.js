2cb3b8c6601bb24d08693bec4aed4623c193d7e9
AnasNabilHasan <162180262+AnasNabilHasan@users.noreply.github.com>
2025-06-02 02:12:07 +0300

    Add script.js

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("helloButton").addEventListener("click", function() {
+  document.getElementById("message").textContent = "Hello! I'm Anas Hasan, a fan of Arsenal, Bayern, and the series Dark!";
+});
